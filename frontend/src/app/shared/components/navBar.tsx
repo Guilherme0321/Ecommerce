97921fb16636efb16b3c6842c7c70ffcced51c80
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-09 21:28:59 -0300

    Esqueleto de navbar

---
+++ frontend/src/app/shared/components/navBar.tsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+    return (
+        <nav className="nav-bar">
+            <ul className="list-items">
+                <li className="item"><a href="home">Home</a></li>
+                <li className="item"><a href="About">About</a></li>
+                <li className="item"><a href="Products">Products</a></li>
+                <li className="item"><a href="Login">Login</a></li>
+            </ul>
+        </nav>
+    )
+}

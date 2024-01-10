c07687c25393346889a3f9f9546297d51cdce744
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-09 21:19:23 -0300

    Adicionado o cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { Routes } from './routes';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors({
+    origin: 'http://localhost:3000',  // Substitua pelo seu domínio frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 app.use(express.json());
 app.use('/', Routes);
 

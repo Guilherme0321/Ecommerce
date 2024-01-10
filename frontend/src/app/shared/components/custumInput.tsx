ea27ec141fe641f1fe29de349590e805067640fb
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-10 14:24:11 -0300

    classeName form-control e ref alterado inputRef

--- frontend/src/app/shared/components/custumInput.tsx
+++ frontend/src/app/shared/components/custumInput.tsx
@@ -1,24 +1,24 @@
 import './css/register.css'
 
 export const Input = (
-    { id, type, labelText, min, required, ref, callFuncion }:{
+    { id, type, labelText, min, required, inputRef, callFuncion }:{
         id: string,
         type: string,
         labelText: string,
         required: boolean,
         callFuncion?:any,
-        ref?: any,
+        inputRef?: any,
         min?:number
     }) => {    
     return (
         <div className='input-group mb-3 d-flex justify-content-center flex-column'>
             <input
                 type={type}
-                className='input-user m-1 border-0 border-bottom rounded-0 bg-transparent border-black'
+                className='input-user form-control m-1 border-0 border-bottom rounded-0 bg-transparent border-black'
                 id={id}
                 onChange={callFuncion}
                 min={min}
-                ref={ref}
+                ref={inputRef}
                 required={required}
             />
             <label className='input-label' htmlFor={id}>{labelText}</label>

b96edb0fe5fe246efa769b9bdbee0a0c8ef8abbf
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-16 19:50:18 -0300

    mudança nas rodas de product e acrescentada rodas de orders

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
 import dbConfig from "./services/dbconfig";
-import { authId, authProductQuery, authUser, authUserName, authUserQuery, authUserEmail } from "./middlewares/validation";
+import { authId, authUser, authUserName, authUserQuery, authUserEmail } from "./middlewares/validationUser";
 import { ProductController } from "./controllers/ProductController";
+import { OrderService } from "./services/OrderService";
+import { authProductQuery } from "./middlewares/validationProduct";
+import { OrderController } from "./controllers/OrderController";
+import { isValidInsertionOrder, validOrderId } from "./middlewares/validationOrder";
 
 export const Routes = Router();
 const usercontroller = new UserController(dbConfig);
 const productcontroller = new ProductController(dbConfig);
+const orderController = new OrderController(dbConfig);
 
 Routes.get('/users', usercontroller.getAllUsers);
 Routes.get('/user/:id', authId, usercontroller.getUserById);
@@ -19,6 +24,9 @@ Routes.post('/user/valid/email', authUserEmail);
 
 Routes.get('/products', productcontroller.getAllProducts);
 Routes.get('/product/:id', authId, productcontroller.getProductById);
-Routes.delete('/product/delete/:id', authId, productcontroller.deleteProductById);
-Routes.post('/product/insert/', authProductQuery, productcontroller.insertProduct);
-Routes.put('/product/update/:id', authId, authProductQuery, productcontroller.updateProductById);
+Routes.delete('/product', authId, productcontroller.deleteProductById);
+Routes.post('/product', authProductQuery, productcontroller.insertProduct);
+Routes.put('/product', authId, authProductQuery, productcontroller.updateProductById);
+
+Routes.post('/orders', isValidInsertionOrder, orderController.insertOrder);
+Routes.delete('/orders', validOrderId, orderController.deleteOrderById);

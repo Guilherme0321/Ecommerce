81e30549621d2e96f8eb2779d7f1133db808e5a7
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-16 19:49:28 -0300

    criado arquivo para validação de cruds de orders

---
+++ backend/src/middlewares/validationOrder.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+
+export const isValidInsertionOrder = (req: Request, res: Response, next: NextFunction) => {
+    const { customer_id, shipping_address, status } = req.body;
+    if(customer_id === undefined) {
+        res.json({ok: false, error: 'Id do usuário é nulo!'});
+    }else if(shipping_address === undefined) {
+        res.json({ok: false, error: 'Endereço é nulo!'})
+    }else if(!['pending', 'paid', 'shipped', 'delivered'].includes(status)) {
+        res.json({ok: false, error: 'status só pode ser pending or paid or shipped or delivered'});
+    }else if(!status === undefined) {
+        res.json({ok: false, error: 'status não pode ser nulo!'});
+    }else {
+        try {
+            next();
+        }catch(error) {
+            res.json(error);
+        }
+    }
+}
+
+export const validOrderId = (req: Request, res: Response, next: NextFunction) => {
+    const { order_id } = req.body;    
+    if(order_id === undefined) {
+        res.json({ok: false, error: 'order_id não pode ser nulo!'});
+    }else {
+        try {
+            next();
+        }catch(error) {
+            res.json(error);
+        }
+    }
+    
+}

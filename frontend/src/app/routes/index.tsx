9ea9d6384cec5c1f5c0c260c27252c4eb68300b9
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-16 19:50:57 -0300

    rota de login

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes as Switch, Navigate } from "react-router-dom";
 import { Home } from "../pages/home";
 import { Register } from "../pages/register";
+import { Login } from "../pages/login";
 
 export const Routes = () => {
     return (
@@ -8,6 +9,7 @@ export const Routes = () => {
             <Switch>
                 <Route path="/home" Component={() => <Home />} />
                 <Route path="/register" Component={() => <Register />} />
+                <Route path="/login" Component={() => <Login />} />
                 <Route path="*" Component={() => <Navigate to="/home" />} />
             </Switch>
         </BrowserRouter>

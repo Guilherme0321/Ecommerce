ffae84560fee57c2a1f3d25dc71d042c29fe6888
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-15 22:06:27 -0300

    forma validação alterada e background de produtos

--- frontend/src/app/pages/register.tsx
+++ frontend/src/app/pages/register.tsx
@@ -1,10 +1,11 @@
 import { useRef, useState } from "react";
 import { Input } from "../shared/components/custumInput";
 import { StatusUser, User } from "../shared/types/user";
-import { userService } from "../shared/service/userService";
-import { validUsername } from "../shared/service/usernameIsUnique";
+import { userService } from "../service/userService";
+import { validUsername } from "../service/usernameIsUnique";
 import { authUser } from "../shared/components/utils/authUserInputs";
-import { validUserEmail } from "../shared/service/userEmailisUnique";
+import { validUserEmail } from "../service/userEmailisUnique";
+import { NavBar } from "../shared/components/navBar";
 export const Register = () => {
 
     const formRef = useRef<HTMLFormElement>(null);
@@ -71,9 +72,18 @@ export const Register = () => {
         return status;
     }
 
-    const setErros = async (statusUser: StatusUser | undefined): Promise<StatusUser | undefined> => {
+    const containsFalse = (statusUser: StatusUser): boolean => {
+        const arrayValue = Object.values(statusUser);
+        for(const value of arrayValue) {
+            if(!value){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const setErros = async (statusUser: StatusUser | undefined): Promise<boolean> => {
         if(statusUser !== undefined) {
-            console.log(statusUser);
             
             if(!statusUser.address?.city) tradeClassValidity(cityRef, false, setLabelCity, 'Invalido');
             else tradeClassValidity(cityRef, true, setLabelCity, 'City');
@@ -93,8 +103,10 @@ export const Register = () => {
             if(!statusUser.email) tradeClassValidity(emailRef, false, setLabelEmail, 'já está sendo utilizado');
             else if(!await checkUserEmail()) statusUser.email = false;
  
+            return containsFalse(statusUser);
+        }else{
+            return false;
         }
-        return statusUser;
     }
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -107,9 +119,9 @@ export const Register = () => {
             address: `${streetRef.current?.value},${numberRef.current?.value},${cityRef.current?.value}`
         }
 
-        const statusUser: StatusUser | undefined = await setErros(authUser(user));
+        const statusUser: boolean = await setErros(authUser(user));
 
-        if(statusUser !== undefined && statusUser?.username && statusUser?.name && statusUser?.email && statusUser.address?.city && statusUser.address?.number && statusUser.address?.street){
+        if(statusUser){
             
             const res = await userService(user)
             console.log(res);
@@ -123,20 +135,25 @@ export const Register = () => {
     }
 
     return (    
-        <div className="position-absolute top-50 start-50 translate-middle bg-white bg-opacity-75 rounded-4" style={{width:'25%'}}>
-            <fieldset className="m-2 position-relative d-flex justify-content-center flex-column">
-                <form ref={formRef} className="form-floating m-3 d-flex justify-content-center flex-column border border-black border-5 p-5 rounded-4" onSubmit={handleSubmit}>
-                        <Input type='text' inputRef={nameRef} labelText={labelName} id='name' required={true} />
-                        <Input type='text' inputRef={emailRef} labelText={labelEmail} id='email' required={true} />
-                        <Input type='text' inputRef={usernameRef} labelText={labelUsername} id='username' required={true} />
-                        <Input type='password' inputRef={passwordRef} labelText='Password' id='password' required={true} />
-                        <Input type='text' inputRef={cityRef} labelText={labelCity} id='city' required={true} />
-                        <Input type='text' inputRef={streetRef} labelText={labelStreet} id='street' required={true} />
-                        <Input type='number' inputRef={numberRef} min={0} labelText={labelNumber} id='number' required={true} />
-                        <button id="submit" className="btn btn-outline-dark" type="submit">Submit</button>
-                </form>
-                <h2 className="position-absolute top-0 bg-black text-white rounded-5 p-1 px-3" style={{left:'5vh'}}>Cadastro</h2>                
-            </fieldset>
-        </div>
+        <>
+            <NavBar />
+            <section className="position-absolute top-50 start-50 translate-middle" id="bg" style={{width: '100%', height:'100%'}}>
+                <div className="position-absolute top-50 start-50 translate-middle bg-white rounded-4" style={{width:'25%'}}>
+                    <fieldset className="m-2 position-relative d-flex justify-content-center flex-column">
+                        <form ref={formRef} className="form-floating m-3 d-flex justify-content-center flex-column border border-black border-5 p-5 rounded-4" onSubmit={handleSubmit}>
+                                <Input type='text' inputRef={nameRef} labelText={labelName} id='name' required={true} />
+                                <Input type='text' inputRef={emailRef} labelText={labelEmail} id='email' required={true} />
+                                <Input type='text' inputRef={usernameRef} labelText={labelUsername} id='username' required={true} />
+                                <Input type='password' inputRef={passwordRef} labelText='Password' id='password' required={true} />
+                                <Input type='text' inputRef={cityRef} labelText={labelCity} id='city' required={true} />
+                                <Input type='text' inputRef={streetRef} labelText={labelStreet} id='street' required={true} />
+                                <Input type='number' inputRef={numberRef} min={0} labelText={labelNumber} id='number' required={true} />
+                                <button id="submit" className="btn btn-outline-dark" type="submit">Submit</button>
+                        </form>
+                        <h2 className="position-absolute top-0 bg-black text-white rounded-5 p-1 px-3" style={{left:'5vh'}}>Cadastro</h2>                
+                    </fieldset>
+                </div>
+            </section>
+        </>
     );
 };

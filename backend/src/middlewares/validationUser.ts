c013219c2751b2faf9f72ddf838305f24d8e55c2
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-16 19:46:37 -0300

    mudado erro de retorno e authProduct colocado em outro arquivo

--- backend/src/middlewares/validationUser.ts
+++ backend/src/middlewares/validationUser.ts
@@ -49,7 +49,7 @@ export const authUser = async (req: Request, res: Response) => {
         if(user !== undefined){
             res.json(user);
         }else{
-            res.json({error: `Nome de usuário ou senha são invalidos!`});
+            res.json({ok: false, error: `Nome de usuário ou senha são invalidos!`});
         }
     }else {
         res.json({error: 'Nome de usuario ou senha são nulo!'});
@@ -78,43 +78,3 @@ export const authUserQuery = async (req: Request, res: Response, next: NextFunct
     }
 }
 
-export const authProductQuery = (req: Request, res: Response, next: NextFunction) => {
-    const { name, description, images, price, stock, categories } = req.body;
-    if(name !== undefined && description !== undefined && images !== undefined && price !== undefined && stock !== undefined && categories !== undefined){
-            const isValidName: boolean =             authName(name.toString());
-            const isValidDescription: boolean =      description.toString().length >= 50;
-            const isValidImages: boolean =           images.toString().split(',').length > 0;
-            const isValidPrice: boolean =            parseFloat(price.toString()) > 0;
-            const isValidCategories: boolean =       categories.toString().split(',').length > 0;            
-            const isValidStock: boolean =            parseInt(stock.toString()) >= 0;
-            if(!isValidName) res.json({
-                error: `Nome invalido!`,
-                details: 'Nome deve ter apenas letras!'
-            });
-            else if(!isValidDescription) res.json({
-                error: `Description invalida!`,
-                details: 'A descrição deve ter pelo menos 50 caracteres!'
-            })
-            else if(!isValidImages) res.json({
-                error: `Imagem invalida!`,
-                details: 'Deve haver pelo menos uma imagem!'
-            })
-            else if(!isValidPrice) res.json({
-                error: `Preço invalido!`,
-                details: 'Preço deve ser maior que 0!'
-            })
-            else if(!isValidCategories) res.json({
-                error: `Categoria invalida`,
-                details: 'Deve haver ao menos uma categoria'
-            })
-            else if(!isValidStock) res.json({
-                error: `Quantidade de estoque invalida!`,
-                details: 'Quantidade de estoque deve ser maior ou igual a 0'
-            })
-            else{
-                next();
-            }
-    }else {
-        res.json({error: 'Os atributos "name", "description", "images", "price", "stock", "categories" não podem ser nulos!'})
-    }
-}

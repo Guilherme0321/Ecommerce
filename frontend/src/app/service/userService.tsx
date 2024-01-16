e3ad78707251e9d3242a20e832dde17d8a2e5404
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-16 19:50:48 -0300

    adiciona auticação do user no front login

--- frontend/src/app/service/userService.tsx
+++ frontend/src/app/service/userService.tsx
@@ -1,7 +1,7 @@
 import { User } from '../shared/types/user'
 
 export const userService = async (user: User): Promise<any> => {
-    const url: string = 'http://localhost:5000/user/insert'
+    const url: string = 'http://localhost:5000/user/insert';
     
     try {
         const res = await fetch(url, {
@@ -16,6 +16,27 @@ export const userService = async (user: User): Promise<any> => {
             throw new Error(`HTTP error! Status: ${res.status}`);
         }
 
+        const response = await res.json();
+        return response;
+    } catch (error) {
+        console.error('Error in userService:', error);
+        return error;
+    }
+}
+
+export const authenticateUser = async (user: Partial<User>): Promise<any> => {
+    const url: string = 'http://localhost:5000/user/authenticate';
+    try{
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        if(!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        }
         const response = await res.json();
         return response;
     } catch (error) {

2bf7f43713c2bc1864f08276ae8f0111b842a59f
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-17 21:23:09 -0300

    criado Card para a pagina de produtos

---
+++ frontend/src/app/shared/components/card.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { getCookie } from "./utils/cookies"
+
+export const Card = (
+    {
+        product_id,image, name, description, price, stock, categories
+    }:{
+        product_id: number,
+        image: string,
+        name: string,
+        description: string,
+        price: number,
+        stock: number,
+        categories?: string[]
+    }
+) => {
+    let [quantity, setQuantity] = useState<number>(0);
+    const user_id = getCookie('user_id');
+    return (
+        <div className="d-flex flex-column justify-content-center g-3 align-items-center" style={{width: '25%'}} id="card">
+            <img src={image} alt={name} className="img-thumbnail rounded-0"/>
+            <div className="d-flex flex-column justify-content-center align-items-center" id="informations">
+                <h3 className="text-center p-1">{name}</h3>
+                <p className="text-center">{description}</p>
+                <p className="fw-bold">R$ <b className=".text-success">{price}</b></p>
+                <div className="d-flex flex-row flex-wrap justify-content-center g-1 align-items-center">
+                    <button onClick={() => {if(quantity > 0) setQuantity(--quantity)}}>-</button>
+                    <p className="text-center">{quantity}</p>
+                    <button onClick={() => setQuantity(++quantity)}>+</button>
+                </div>
+                <p>Stock: {stock}</p>
+                <div className="categories">
+                    {categories?.map((h) => (
+                        <p>{h}</p>
+                    ))}
+                </div>
+                <button onClick={() => console.log({user_id: user_id, product_id: product_id, quantity: quantity})}>Add to cart</button>
+            </div>
+        </div>
+    )
+}

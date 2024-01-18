a0a78f7cf7396485da592a6d72aabe4d5f5410c4
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-17 21:21:07 -0300

    criada pagina de produtos não ainda não estilisada

---
+++ frontend/src/app/pages/products.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+import { NavBar } from "../shared/components/navBar"
+import { getAllProduct } from "../service/productService";
+import { Product } from "../shared/types/product";
+import { Card } from "../shared/components/card";
+
+export const Products = () => {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<Product[]>([{
+        name: "",
+        categories: [],
+        images: [],
+        description: "",
+        price: 0,
+        product_id: 0,
+        stock: 0,
+        variants: []
+    }]);
+    
+    useEffect(() => {        
+        const getProducts = async () => {
+            try {
+                const res = await getAllProduct();
+                if(res !== undefined){
+                    setData(res);
+                }
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            } 
+        }
+        getProducts();
+    }, []);
+
+    return (
+        <>
+            <NavBar />
+            <section className="d-flex flex-row flex-wrap justify-content-center m-5 gap-2 py-2">
+                {loading ? (
+                    <h1>Loading</h1>
+                ) : (
+                    data.map( data => (<Card 
+                        key={data.product_id} 
+                        product_id={data.product_id}
+                        image={data.images[0]}
+                        name={data.name}
+                        description={data.description}
+                        price={data.price}
+                        stock={data.stock}
+
+                    />))
+                )}
+            </section>
+        </>
+    )
+}

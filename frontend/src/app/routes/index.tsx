9afac7896ce1ea550e4fa1586aed701fd13fe1f3
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-17 21:22:12 -0300

    alteração de acesso pra rotas a partir dos cookies

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Route, Routes as Switch, Navigate } from "react-router-d
 import { Home } from "../pages/home";
 import { Register } from "../pages/register";
 import { Login } from "../pages/login";
+import { getCookie } from "../shared/components/utils/cookies";
+import { Products } from "../pages/products";
 
 export const Routes = () => {
+    const isAuthenticated = getCookie('user_id');
+    console.log(isAuthenticated);
+    
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/home" Component={() => <Home />} />
-                <Route path="/register" Component={() => <Register />} />
-                <Route path="/login" Component={() => <Login />} />
+                {isAuthenticated === null && <Route path="/register" Component={() => <Register />} />}
+                {isAuthenticated === null && <Route path="/login" Component={() => <Login />} />}
+                {isAuthenticated && <Route path="/products" Component={() => <Products />} />}
                 <Route path="*" Component={() => <Navigate to="/home" />} />
             </Switch>
         </BrowserRouter>

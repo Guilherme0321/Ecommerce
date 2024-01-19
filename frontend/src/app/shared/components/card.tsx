69c74594c3bd8647dcf489c7286e56b1c2b66daf
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-19 10:55:43 -0300

    requisição para adicionar no carrinho

--- frontend/src/app/shared/components/card.tsx
+++ frontend/src/app/shared/components/card.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { getCookie } from "./utils/cookies"
+import { insertToCart } from "../../service/cartItemsService";
 
 export const Card = (
     {
@@ -16,25 +17,38 @@ export const Card = (
 ) => {
     let [quantity, setQuantity] = useState<number>(0);
     const token =  getCookie('token');
+
+    const addToCartItems = async () => {
+        if(token === null) {
+            console.log('token invalido!');
+        }else {
+            const res = await insertToCart(product_id, quantity, token);
+            console.log(res);
+        }
+    }
     return (
-        <div className="d-flex flex-column justify-content-center g-3 align-items-center" style={{width: '25%'}} id="card">
-            <img src={image} alt={name} className="img-thumbnail rounded-0"/>
-            <div className="d-flex flex-column justify-content-center align-items-center" id="informations">
-                <h3 className="text-center p-1">{name}</h3>
-                <p className="text-center">{description}</p>
-                <p className="fw-bold">R$ <b className=".text-success">{price}</b></p>
-                <div className="d-flex flex-row flex-wrap justify-content-center g-1 align-items-center">
-                    <button onClick={() => {if(quantity > 0) setQuantity(--quantity)}}>-</button>
-                    <p className="text-center">{quantity}</p>
-                    <button onClick={() => setQuantity(++quantity)}>+</button>
+        <div className="col">
+            <div className="card" id="card">
+                <img src={image} alt={name} className="card-img-top"/>
+                <div className="card-body" id="informations">
+                    <h3 className="card-title text-center p-1">{name}</h3>
+                    <p className="card-text text-center">{description}</p>
+                    <p className="card-text fw-bold">R$ <b className=".text-success">{price}</b></p>
+                    <div className="d-flex flex-row flex-wrap justify-content-center align-items-center">
+                        <button className="bg-white border-1 rounded-3 text-center" onClick={() => {if(quantity > 0) setQuantity(--quantity)}}>-</button>
+                        <p className="text-center my-0 mx-3">{quantity}</p>
+                        <button className="bg-white border-1 rounded-3 text-center" onClick={() => setQuantity(++quantity)}>+</button>
+                    </div>
+                    <p>Stock: {stock}</p>
+                    <div className="d-flex flex-row flex-wrap gap-1">
+                        {categories?.map((h) => (
+                            <p key={h}>{h}</p>
+                        ))}
+                    </div>
+                    <div className="card-footer">
+                        <button style={{width: '100%'}} className="bg-danger border-0" onClick={addToCartItems}>Add to cart</button>
+                    </div>
                 </div>
-                <p>Stock: {stock}</p>
-                <div className="categories">
-                    {categories?.map((h) => (
-                        <p>{h}</p>
-                    ))}
-                </div>
-                <button onClick={() => console.log({product_id: product_id, quantity: quantity, token: token})}>Add to cart</button>
             </div>
         </div>
     )

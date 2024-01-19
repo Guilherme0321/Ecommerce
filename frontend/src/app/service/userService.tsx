fa276a7cd1ff811fd96ab7044a55cf6f062abc2e
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-19 10:55:28 -0300

    checagem de email e nome

--- frontend/src/app/service/userService.tsx
+++ frontend/src/app/service/userService.tsx
@@ -64,3 +64,49 @@ export const logout = async () => {
         return error;
     }
 }
+
+export const validUserEmail = async (email: string): Promise<any> => {
+    const url = 'http://localhost:5000/user/valid/email';
+    try {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: email})
+        });
+
+        if(!res.ok) {
+            throw Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        const response = await res.json();
+        return response;
+    }catch(error){
+        console.error('Error in userService:', error);
+        return error;
+    }
+}
+
+export const validUsername = async (username: string): Promise<any> => {
+    const url = 'http://localhost:5000/user/valid/username'
+    try {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username: username})
+        });
+
+        if(!res.ok) {
+            throw Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        const response = await res.json();
+        return response;
+    }catch(error){
+        console.error('Error in userService:', error);
+        return error;
+    }
+}

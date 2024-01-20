61c12bcf0d52b6a638e1377b50bfee163cf9a8f6
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-01-20 09:37:02 -0300

    resposta ao adicionar item ao carrinho

--- frontend/src/app/shared/components/card.tsx
+++ frontend/src/app/shared/components/card.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getCookie } from "./utils/cookies"
 import { insertToCart } from "../../service/cartItemsService";
-
+import '../styles/products.css'
 export const Card = (
     {
         product_id,image, name, description, price, stock, categories
@@ -16,14 +16,46 @@ export const Card = (
     }
 ) => {
     let [quantity, setQuantity] = useState<number>(0);
+    let [buttonText, setButtonText] = useState<string>('Add to Cart');
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
     const token =  getCookie('token');
+
+    const changeButtonBgColor = () => {
+        const classList = buttonRef.current?.classList;
+        if(classList?.contains('bg-danger')){
+            classList.remove('bg-danger');
+            classList.add('bg-success');
+        }else{
+            classList?.add('bg-danger');
+            classList?.remove('bg-sucess');
+
+        }
+    }
+
+    const changeButtonText = () => {
+        setButtonText('Added to Cart');
+        changeButtonBgColor();
+        setTimeout(() => {
+            setButtonText('Add to Cart');
+            changeButtonBgColor();
+        },1000)
+    }
 
     const addToCartItems = async () => {
         if(token === null) {
             console.log('token invalido!');
         }else {
-            const res = await insertToCart(product_id, quantity, token);
-            console.log(res);
+            if(quantity > 0){
+                const res = await insertToCart(product_id, quantity, token);
+                console.log(res);
+                if(res.ok) {
+                    changeButtonText();
+                }
+            } else {
+                console.log('Quantidade não pode ser 0!');
+                
+            }
         }
     }
     return (
@@ -40,13 +72,13 @@ export const Card = (
                         <button className="bg-white border-1 rounded-3 text-center" onClick={() => setQuantity(++quantity)}>+</button>
                     </div>
                     <p>Stock: {stock}</p>
-                    <div className="d-flex flex-row flex-wrap gap-1">
+                    <div className="d-flex flex-row gap-1">
                         {categories?.map((h) => (
                             <p key={h}>{h}</p>
                         ))}
                     </div>
                     <div className="card-footer">
-                        <button style={{width: '100%'}} className="bg-danger border-0" onClick={addToCartItems}>Add to cart</button>
+                        <button style={{width: '100%'}} ref={buttonRef} className="bg-danger border-0" onClick={addToCartItems}>{buttonText}</button>
                     </div>
                 </div>
             </div>
